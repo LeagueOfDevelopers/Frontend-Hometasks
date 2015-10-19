c667b0337c604d852d6be7df65add3dba71f1bda
Twllerm <skyteether@gmail.com>
2015-10-20 01:01:29 +0300

    Task 5 выполнен

--- Lesson 1/Task 5/task.js
+++ Lesson 1/Task 5/task.js
@@ -1,1 +1,23 @@
 // your code goes here
+
+function makeLine() {
+
+  var students = [];
+  
+  function student(i) {
+  		return function() {
+  			console.log(i);
+  	}
+  };
+
+  for(var i = 0; i < 10; i++) {
+  	students.push(student(i));
+  }
+
+  return students;
+}
+
+var line = makeLine();
+
+line[0](); 
+line[5](); 

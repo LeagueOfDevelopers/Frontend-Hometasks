822bc9ea56535e64e7dbc15cb56e944ccd5cb2ad
Twllerm <skyteether@gmail.com>
2015-10-20 01:16:39 +0300

    Task 1  с дополнением

--- Lesson 1/Task 1/task.js
+++ Lesson 1/Task 1/task.js
@@ -1,9 +1,23 @@
 // your code goes here
 
-var sum = function(a) {
+var sumOfTwo = function(a) {
 	return function(b) {
 		return a+b
 	}
 }
 
-console.log(sum(4)(6));
+console.log(sumOfTwo(4)(6));
+
+// Дополнение
+
+ var sumOfAll = function(a) {
+ 	var sum = function(item) {
+ 		if(item != "=") {
+ 		a += item;
+ 		return sum;
+ 		} else {return a};
+ 	}
+ 	return sum;
+ }
+
+ console.log(sumOfAll(2)(4)(10)(29)(12)(14)('='));
